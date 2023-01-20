3f978ec974aa2d2eb765b5bb8b90f307594abc26
Maksim <m.bondarenko1988@yandex.ru>
2023-01-20 20:06:15 +0300

    Done third homework to second seminar about C#

---
+++ homework_3/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+System.Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch(number){
+    case 1:{
+        System.Console.WriteLine("Понедельник");
+        break;
+    }
+    case 2:{
+        System.Console.WriteLine("Вторник");
+        break;
+    }
+    case 3:{
+        System.Console.WriteLine("Среда");
+        break;
+    }
+    case 4:{
+        System.Console.WriteLine("Четверг");
+        break;
+    }
+    case 5:{
+        System.Console.WriteLine("Пятница");
+        break;
+    }
+    case 6:{
+        System.Console.WriteLine("Суббота");
+        break;
+    }
+    case 7:{
+        System.Console.WriteLine("Воскресенье");
+        break;
+    }
+    default:{
+        System.Console.WriteLine("Такого дня нет");
+        break;
+    }
+}
