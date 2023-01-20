c57dc4e36ba66b3bcb726199400d770297e4deea
Maksim <m.bondarenko1988@yandex.ru>
2023-01-20 19:51:03 +0300

    Done second homework to second seminar about C#

---
+++ homework_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+System.Console.Write("Введите заданное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number >= 1000) number /= 10;{
+    System.Console.WriteLine(number % 10);
+}
